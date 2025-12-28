83d9ebe04551c200b5f45616aa9151e90a9571ed
Kapil Satyarthi <Kapilsatyarthi878787@gmail.com>
2025-12-28 23:43:58 +0530

    Fix admin login UI and auth

--- frontend/src/components/AdminProtectedRoute.jsx
+++ frontend/src/components/AdminProtectedRoute.jsx
@@ -1,9 +1,31 @@
 import { Navigate } from "react-router-dom";
+import { useState } from "react";
+import jwtDecode from "jwt-decode";
 
 const AdminProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("adminToken");
+  const [authorized] = useState(() => {
+    const token = localStorage.getItem("adminToken");
 
-  if (!token) {
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const decoded = jwtDecode(token);
+      const now = Date.now();
+
+      if (decoded.exp * 1000 < now) {
+        localStorage.removeItem("adminToken");
+        return false;
+      }
+      return true;
+    } catch {
+      localStorage.removeItem("adminToken");
+      return false;
+    }
+  });
+
+  if (!authorized) {
     return <Navigate to="/admin/login" replace />;
   }
 
